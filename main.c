c3208e2fce1bc91e0d1301271724f3dd013f5d53
ilya-v <ilya.volkovets@gmail.com>
2013-10-13 23:58:17 +0200

    Added dongle reset before operation

--- main.c
+++ main.c
@@ -6,17 +6,16 @@
 #include "cmd_def.h"
 
 #define ENSURE(cmd, err) do { \
-        if(!(cmd)) {  printf("%s: %d\n", (err),(int)GetLastError());  exit(-1);  }\
+        if(!(cmd)) {  printf("%s: %d\n", (err),(int)GetLastError());  exit(-1);  } else if (0) {printf("%s: OK\n", #cmd);}\
 } while(0);
 
+HANDLE serial_handle;
 
-volatile HANDLE serial_handle;
-
-void output(uint8 len1,uint8* data1,uint16 len2,uint8* data2)
+void output(uint8 len1, uint8* data1, uint16 len2, uint8* data2)
 {
     DWORD written;
-    ENSURE(WriteFile (serial_handle, data1, len1, &written, NULL),  "ERROR: Writing data.");
-    ENSURE(WriteFile (serial_handle, data2, len2, &written, NULL),  "ERROR: Writing data.");
+    ENSURE(WriteFile (serial_handle, data1, len1, &written, NULL),  "ERROR: Writing data.1");
+    ENSURE(WriteFile (serial_handle, data2, len2, &written, NULL),  "ERROR: Writing data.2");
 }
 
 int read_message()
@@ -34,46 +33,45 @@ int read_message()
 
     //read rest if needed
     if(apihdr.lolen)
-    {
         if(!ReadFile(serial_handle, data, apihdr.lolen, &rread, NULL))
             return GetLastError();
-    }
 
-    apimsg=ble_get_msg_hdr (apihdr);
+    apimsg = ble_get_msg_hdr (apihdr);
     if(!apimsg)
     {
         printf("ERROR: Message not found:%d:%d\n",(int)apihdr.cls,(int)apihdr.command);
         return -1;
     }
     apimsg->handler(data);
-
     return 0;
+}
+
+HANDLE open_serial_port (const char *port_name_arg)
+{
+    char serial_port_name[80];
+    HANDLE serial_handle = INVALID_HANDLE_VALUE;
+    snprintf(serial_port_name, sizeof(serial_port_name)-1, "\\\\.\\%s", port_name_arg);
+    
+    for (int attempt_count = 20; attempt_count > 0 && serial_handle == INVALID_HANDLE_VALUE; attempt_count --)
+    {
+        Sleep(500);    
+        serial_handle  =  CreateFileA (serial_port_name, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    } 
+    return  serial_handle;
 }
 
 int main(int argc, char *argv[] )
 {
-    char str[80];
-
     bglib_output = output;
-
-    if(argc<2)  exit(-1);
-
-    snprintf(str,sizeof(str)-1,"\\\\.\\%s",argv[1]);
-    serial_handle = CreateFileA(str,
-                                   GENERIC_READ | GENERIC_WRITE,
-                                   FILE_SHARE_READ|FILE_SHARE_WRITE,
-                                   NULL,
-                                   OPEN_EXISTING,
-                                   FILE_ATTRIBUTE_NORMAL,
-                                   NULL);
-
-
+    if ( argc < 2 )  exit(-1);
+    
+    serial_handle = open_serial_port( argv[1] );
+    //ble_cmd_system_reset(0);
+    //serial_handle = open_serial_port( argv[1] );
     ENSURE (serial_handle != INVALID_HANDLE_VALUE, "Error opening serial port");
 
-    //stop previous operation
-    ble_cmd_gap_end_procedure();
-    //get connection status,current command will be handled in response
-    ble_cmd_connection_get_status(0);
+    ble_cmd_gap_end_procedure();         //stop previous operation
+    ble_cmd_connection_get_status(0);    //get connection status,current command will be handled in response
 
     for (; !read_message(); );
     return 0;
