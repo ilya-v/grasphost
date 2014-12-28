5f2bfed893ba72e150e1d1a900ba2da137ecd143
ilya-v <ilya.volkovets@gmail.com>
2014-12-28 23:15:43 +0100

    Key Event simulation for Mac is implemented

---
+++ key_event_mac.cpp
@@ -0,0 +1,21 @@
+#if defined(__APPLE__) && defined(__MACH__)
+
+#include <ApplicationServices/ApplicationServices.h>
+
+//http://web.archive.org/web/20100501161453/http://www.classicteck.com/rbarticles/mackeyboard.php
+
+namespace {
+    void simulate_key_event_mac(const unsigned key_code, const bool is_push) {
+
+        CGEventSourceRef source = CGEventSourceCreate(kCGEventSourceStateCombinedSessionState);
+        CGEventRef cmd = CGEventCreateKeyboardEvent(source, (CGKeyCode)key_code, is_push?YES:NO);
+        CGEventPost(kCGAnnotatedSessionEventTap, cmd);
+        CFRelease(cmd);
+        CFRelease(source);
+    }
+}
+
+void simulate_key_press(const unsigned key_code) { simulate_key_event_mac(key_code, true); }
+void simulate_key_release(const unsigned key_code) { simulate_key_event_mac(key_code, false); }
+
+#endif
