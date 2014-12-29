33f9aec68a0687fd8f5ca1d7ca2b1a52d484aae8
ilya-v <ilya.volkovets@gmail.com>
2014-12-30 00:11:21 +0100

    Diag

--- main.cpp
+++ main.cpp
@@ -326,12 +326,14 @@ int main(int argc, char *argv[] )
     static SerialPort  serial_port;
 
     bglib_output = [](uint8_t len1, uint8_t* data1, uint16_t len2, uint8_t* data2)  {
-        printf("\n");
+        printf(">\n");
         for (uint8_t *p = data1; p < data1 + len1; p++)
             printf(".%02X", (unsigned)*p);
         printf(";");
         for (uint8_t *p = data2; p < data2 + len2; p++)
             printf(".%02X", (unsigned)*p);
+        printf("\n");
+        fflush(stdout);
         serial_port.Write(data1, len1);
         serial_port.Write(data2, len2);
     };
