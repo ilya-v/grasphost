a50f03dd61300d03fdecba64a0b0406eae414846
ilya-v <ilya.volkovets@gmail.com>
2015-01-02 21:59:13 +0100

    Key filtering function translated to plain C

--- key_filter.cpp
+++ key_filter.cpp
@@ -9,95 +9,63 @@ namespace
     bool  are_prev_levels_set  =  false;
 }
 
-enum {
-    key_0_pressed = 1 << 0,
-    key_1_pressed = 1 << 1,
-    key_2_pressed = 1 << 2,
-    key_3_pressed = 1 << 3,
-    key_4_pressed = 1 << 4
-};
 
-/*
-int filter_keys(const int adc[5])
-{
-    static int 
-        PF_set = 0,                 // are forces from the previous iteration set
-        PF[5] = { 0, 0, 0, 0, 0 };  // forces from previous iteration; stored between calls to this function
+namespace {
+    int max(int a, int b) { return a > b ? a : b; }
 
-    const int // F is array of pressure forces on each sensor, in range [0, 127]
-        F[5] = { 0x7F - adc[0], 0x7F - adc[1], 0x7F - adc[2], 0x7F - adc[3], 0x7F - adc[4] };
+    const int
+        LT[5] = { 10, 10, 15, 15, 15 },
+        DT[5] = { 3, 4, 4, 5, 5 };
 
-    int D[5] = { 0, 0, 0, 0, 0 }; // Delta between current forces (F) and previous step forces (PF)
-    if (PF_set) {
-        D[0] = F[0] - PF[0]; D[1] = F[1] - PF[1]; D[2] = F[2] - PF[2]; D[3] = F[3] - PF[3]; D[4] = F[4] - PF[4];
+#define FORALL for(unsigned i = 0; i < 5; i++)
+
+    void filter_keys(const int adc[5], int pressed[5])
+    {
+        static int
+            PF_set = 0,                 // are forces from the previous iteration set
+            PF[5] = { 0, 0, 0, 0, 0 };  // forces from previous iteration; stored between calls to this function
+
+        int F[5];// F is array of pressure forces on each sensor, with elements in range of [0, 127]
+        FORALL F[i] = 0x7F - adc[i];
+
+        int D[5] = { 0, 0, 0, 0, 0 };   // Delta between the current forces (F) and previous step forces (PF)
+        if (PF_set)                     //only calculate Delta if the previous forces are set
+            FORALL D[i] = F[i] - PF[i];
+
+        int S[5] = { 0, 0, 0, 0, 0 };   // Suppress flag defines which detected key presses to suppress 
+        {
+            const int d2 = max(D[2] - DT[2], 0), d3 = max(D[3] - DT[3], 0), d4 = max(D[4] - DT[4], 0);
+            const int dmax = max(d2, max(d3, d4));
+
+            if (dmax >= 0) {  // from the sensors 2, 3 and 4 (starting from 0), only one counts as pressed: the one with the maximum Delta
+                S[2] = S[3] = S[4] = 1;
+                if (dmax == d4)  S[4] = 0; else if (dmax == d3) S[3] = 0; else S[2] = 0;
+            }
+        }
+
+        FORALL pressed[i] = pressed[i] || (D[i] > DT[i] && !S[i]);
+        FORALL pressed[i] = pressed[i] && F[i] > LT[i];
+
+        FORALL PF[i] = F[i];            // store current forces for the next iteration
+        PF_set = 1;                     // "previous forces" were set for the next iteration
     }
-
-    int S[5] = { 0, 0, 0, 0, 0 }; // Suppress flag: which detected key presses to suppress 
-
-    const int d2 = 
-
-
-    // store current forces for the next iteration
-    PF[0] = F[0]; PF[1] = F[1]; PF[2] = F[2]; PF[3] = F[3]; PF[4] = F[4];
-    // "previous forces" were set for the next iteration
-    PF_set = 1;
-    
 }
-*/
-
 std::array<unsigned, 10>  process_sensor_levels(const config_t & config, const uint8_t levels[5])
 {
-    std::array<unsigned, 5>  original_levels{};
-    std::transform(levels, levels + 5, original_levels.begin(), [](uint8_t l) { return 0x7F - l; });
-
-    if (!are_prev_levels_set) {
-        prev_levels = original_levels;
-        are_prev_levels_set = true;
-    }
-
-    std::array<int, 5>   delta_levels{};
-    for  (unsigned i = 0; i < 5; i++)
-    {
-        delta_levels[i] = original_levels[i] - prev_levels[i];
-    }
-    prev_levels = original_levels;
-
-    std::array<bool, 5>  suppress{};
-    {
-        const int
-            delta_2 = std::max(delta_levels[2] - (int)config.delta_thresholds[2], 0),
-            delta_3 = std::max(delta_levels[3] - (int)config.delta_thresholds[3], 0),
-            delta_4 = std::max(delta_levels[4] - (int)config.delta_thresholds[4], 0),
-            max_delta = std::max({ delta_2, delta_3, delta_4 });
-
-        if (max_delta >= 0)
-        {
-            suppress[2] = suppress[3] = suppress[4] = true;
-            const int  idx_max = ((delta_4 == max_delta) ? 4 : (delta_3 == max_delta) ? 3 : 2);
-            suppress[idx_max] = false;
-        }
-    }
+    const int adc[5] = { levels[0], levels[1], levels[2], levels[3], levels[4] };
+    int pressed[5] = { 0, };
+    filter_keys(adc, pressed);    
     
-    std::array<unsigned, 5>  filtered_levels{};
-    std::array<bool, 5> active_sensors = key_states;
-    for (unsigned i = 0; i < 5; i++)
-    {
-        if (original_levels[i] > config.level_thresholds[i] && delta_levels[i] >(int)config.delta_thresholds[i]  && ! suppress[i])
-        {
-            active_sensors[i] = true;
-        }   
-
-        if (active_sensors[i] && original_levels[i] <= config.level_thresholds[i])
-        {
-            active_sensors[i] = false;
-        }
-
-        filtered_levels[i] = active_sensors[i]?original_levels[i] : 0;
-    }    
+    std::array < unsigned, 5 >
+        filtered_levels = {},
+        original_levels = {};
+    for (unsigned i = 0; i < 5; i++) original_levels[i] = 0x7F - adc[i];
+    for (unsigned i = 0; i < 5; i++) filtered_levels[i] = (pressed[i] ? original_levels[i] : 0);
+    
 
     for (unsigned i = 0; i < 5; ++i)
-        if (key_states[i] != active_sensors[i])
-            ((key_states[i] = active_sensors[i]) ? simulate_key_press : simulate_key_release)(config.key_codes[i]);
+        if (key_states[i] != !! pressed[i])
+            ((key_states[i] = !! pressed[i]) ? simulate_key_press : simulate_key_release)(config.key_codes[i]);
 
     std::array<unsigned, 10>  result{};
     std::copy_n(original_levels.begin(), 5, result.begin() + 0);
